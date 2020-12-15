8bbeefb917294923f952b7e704bcba7d090d0776
edmundo papi <edmundopierre@yahoo.com>
2020-12-15 22:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials.map(tutorial =>tutorial.split(" "))
+  return tutorials.map(tutorial =>{
+    tutorial.split(" ").map( word => word.charAt(0).toUpperCase() + word.slice(1))
+    )
+    
+  }
+)
 }
